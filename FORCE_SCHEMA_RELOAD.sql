f9663764db751add5aa2b6dea1d1db89258ad538
Jasper Lepardo <jsprlprd@gmail.com>
2025-08-04 21:21:15 +0800

    cleanup: remove temporary logout.html file
    - Remove temp-logout.html as it's a temporary debugging file
    - App already has proper logout functionality in AuthContext
    - Reduces security risk of exposed logout logic

--- FORCE_SCHEMA_RELOAD.sql
+++
@@ -1,7 +0,0 @@
--- Force PostgREST schema reload
-NOTIFY pgrst, 'reload schema';
-
--- Also try reloading the config
-NOTIFY pgrst, 'reload config';
-
-SELECT 'Schema reload signals sent' as result;
--- VERIFY_SCHEMA.sql
+++
@@ -1,9 +0,0 @@
--- Verify the actual database schema
-SELECT 'Households table columns:' as info;
-SELECT column_name, data_type, is_nullable 
-FROM information_schema.columns 
-WHERE table_name = 'households' 
-ORDER BY ordinal_position;
-
-SELECT 'Sample household data:' as info;
-SELECT * FROM households LIMIT 1;
